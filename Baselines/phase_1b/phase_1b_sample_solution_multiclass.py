a4ad305938b16e2714d68f580349f7aa7a1fef8b
V-bot29 <110687591+V-bot29@users.noreply.github.com>
2025-11-30 18:14:21 +0100

    i dont understand git

--- Baselines/phase_1b/phase_1b_sample_solution_multiclass.py
+++ Baselines/phase_1b/phase_1b_sample_solution_multiclass.py
@@ -73,8 +73,8 @@ def main():
                 # transforms.Normalize(mean=[0.485, 0.456, 0.406],
                 #                     std=[0.229, 0.224, 0.225]), # TODO calculate dataset mean and std
 
-                transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225]),
+                #transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     #std=[0.229, 0.224, 0.225]),
 
             ])
         
